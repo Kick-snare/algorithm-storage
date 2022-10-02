257be99e9bb7f81fbfcb33e428a1a61f4204ea5c
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2022-10-03 00:34:09 +0900

    [level 3] Title: 불량 사용자, Time: 0.01 ms, Memory: 3.57 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,57 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<string> users, bans;
 set<string> s;
-int cnnt =0;
+
 
 bool isCase(string &user, string &ban) {
-    if(user.length() != ban.length()) return false;
+    if(user.length() != ban.length()) 
+        return false;
     
     for(int i=0;i<user.length();i++) 
-        if(user[i] != ban[i] && ban[i] != '*') return false;
+        if(user[i] != ban[i] && ban[i] != '*') 
+            return false;
     
     return true;
 }
 
-void bt(vector<string> &user_id, vector<string> &banned_id, vector<int> &visited, int ban_idx) {
+void bt(vector<int> &visited, int idx) {
 
-    if(ban_idx == banned_id.size()) {
-        cnnt++;
+    if(idx == bans.size()) {
         string str = "";
-        for(int i=0;i<user_id.size();i++) if(visited[i]) str += user_id[i];
+        
+        for(int i=0;i<users.size();i++) 
+            if(visited[i]) 
+                str += users[i];
         s.insert(str);
-        // cout << str << endl;
+
         return;
     }
     
-    for(int i=0;i<user_id.size();i++) {
-        if(isCase(user_id[i], banned_id[ban_idx]) && !visited[i]
-           // find(path.begin(), path.end(), user_id[i]) == path.end()
-          ) {
+    for(int i=0;i<users.size();i++) {
+        if(isCase(users[i], bans[idx]) && !visited[i]) {
 
-            // path.push_back(user_id[i]);
             visited[i] = true;
-            bt(user_id, banned_id, visited, ban_idx+1);
+            bt(visited, idx+1);
             visited[i] = false;
-            // path.pop_back();
         }
     }
     return;
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-    map<string, int> cnt;
+    users = user_id;
+    bans  = banned_id;
     
-    for(auto &id: banned_id)
-        if(cnt.count(id)) cnt[id]++;
-        else cnt[id] = 1;
-    
-    // vector<string> arr;
     vector<int> arr(8, false);
-    bt(user_id, banned_id, arr, 0);
-    
-    for(auto &p: cnt)
-        if(p.second > 1) 
-            cnnt /= p.second;
+    bt(arr, 0);
 
     return s.size();
 }

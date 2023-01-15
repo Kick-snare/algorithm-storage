8b308ca6016b1ba17b98cf7635ed115012e99c0a
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2023-01-15 16:44:23 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 384 ms, Memory: 118100 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,42 +1,30 @@
-import sys, math
-
-input = sys.stdin.readline
-dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
-R, C, T = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(R)]
-
-upper = -1
-down = -1
-
-for i in range(R):
-    if arr[i][0] == -1:
-        upper, down = i, i+1
-        break
+import sys
 
 def isIn(i, j): return 0 <= i and i < R and 0 <= j and j < C
 def isAC(i, j): return j == 0 and (i == upper or i == down)
 
-def diffuse_dust(arr):
+def diffuse_dust():
+    global arr
     newarr = [tmp[:] for tmp in arr]
     
     for i in range(R):
         for j in range(C):
-            if arr[i][j]==0 or isAC(i, j): continue
-            dust_sum = 0
+            if arr[i][j] <= 0: continue
+            curr_val = arr[i][j] // 5
+            cnt = 0
             for k in range(4):
-                y, x = i + dy[k], j + dx[k]
+                y, x = i+dy[k], j+dx[k]
+                if isAC(y, x) or not isIn(y, x): continue
                 
-                if isAC(y, x) or not isIn(y, x):
-                    continue
-                
-                value = math.floor(arr[i][j] / 5)
-                newarr[y][x] += value
-                dust_sum += value
+                newarr[y][x] += curr_val
+                cnt += 1
 
-            newarr[i][j] -= dust_sum
-    return newarr
+            newarr[i][j] -= curr_val * cnt
+    arr = newarr
+    return
 
-def blow_wind(arr):
+def blow_wind():
+    global arr
     newarr = [tmp[:] for tmp in arr]
     newarr[upper][1] = 0
     newarr[down][1] = 0
@@ -58,19 +46,28 @@ def blow_wind(arr):
         newarr[R-1][j-1] = arr[R-1][j]
     for i in range(R-1, down+1, -1):
         newarr[i-1][0] = arr[i][0]
+    arr = newarr
+    return
 
-    return newarr
+input = sys.stdin.readline
+dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+R, C, T = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(R)]
+upper, down = -1, -1
 
-def get_total_dust(arr):
-    total_sum = 0
-    for i in range(R):
-        for j in range(C):
-            if isAC(i, j): continue
-            total_sum += arr[i][j]
-    return total_sum
+for i in range(R):
+    if arr[i][0] == -1:
+        upper, down = i, i+1
+        break
 
 for i in range(T):
-    arr = diffuse_dust(arr)
-    arr = blow_wind(arr)
-# for i in arr: print(i)
-print(get_total_dust(arr))     
+    diffuse_dust()
+    blow_wind()
+
+total_sum = 0
+for i in range(R):
+    for j in range(C):
+        if arr[i][j] == -1: continue
+        total_sum += arr[i][j]
+
+print(total_sum)

11b62975531131be00d7292def2583041ffda61d
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2023-04-17 00:01:44 +0900

    [Silver II] Title: 마인크래프트, Time: 876 ms, Memory: 47268 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.kt
@@ -0,0 +1,52 @@
+private fun readStrings() = readln().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+
+data class Candidate(
+    val time: Int,
+    val height: Int,
+) : Comparable<Candidate> {
+    override fun compareTo(other: Candidate): Int {
+        return if(time == other.time) height - other.height
+        else other.time - time
+    }
+}
+
+fun main() {
+    var maxHeight = 0
+    var minHeight = 300
+    val (n, m, b) = readInts()
+    val arr = Array(n) { IntArray(m) }
+    
+    repeat(n) { i ->
+        readln().split(' ').forEachIndexed { j, h ->
+            val height = h.toInt()
+            maxHeight = maxHeight.coerceAtLeast(height)
+            minHeight = minHeight.coerceAtMost(height)
+            arr[i][j] = height
+        }
+    }
+
+    var candidate = Candidate(987654321, 0 )
+
+    for(height in minHeight..maxHeight) {
+        var stackCount = 0
+        var popCount = 0
+
+        for(i in 0 until n) {
+            for(j in 0 until m) {
+                val target = arr[i][j]
+                when {
+                    target < height -> stackCount += height - target
+                    target > height -> popCount += target - height
+                }
+            }
+        }
+
+        val time = stackCount + popCount * 2
+
+        if(b + popCount >= stackCount)
+            candidate = candidate.coerceAtLeast(Candidate(time, height))
+    }
+
+    with(candidate) { print("$time $height") }
+}

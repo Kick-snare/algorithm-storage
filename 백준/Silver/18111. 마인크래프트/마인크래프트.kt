7cbb34804b208f19242da38a77120e34b6fc2261
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2023-04-17 00:19:38 +0900

    [Silver II] Title: 마인크래프트, Time: 836 ms, Memory: 47620 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.kt
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.kt
@@ -1,17 +1,5 @@
-import java.util.StringTokenizer
-
 private fun readStrings() = readln().split(" ")
 private fun readInts() = readStrings().map { it.toInt() }
-
-data class Candidate(
-    val time: Int,
-    val height: Int,
-) : Comparable<Candidate> {
-    override fun compareTo(other: Candidate): Int {
-        return if(time == other.time) height - other.height
-        else other.time - time
-    }
-}
 
 fun main() {
     var maxHeight = 0
@@ -20,24 +8,22 @@ fun main() {
     val arr = Array(n) { IntArray(m) }
 
     repeat(n) { i ->
-        val st = StringTokenizer(readln())
-        repeat(m) { j ->
-            val height = st.nextToken().toInt()
+        readln().split(' ').forEachIndexed { j, h ->
+            val height = h.toInt()
             maxHeight = maxHeight.coerceAtLeast(height)
             minHeight = minHeight.coerceAtMost(height)
             arr[i][j] = height
         }
     }
 
-    var candidate = Candidate(987654321, 0 )
+    var candidate = Pair(Int.MAX_VALUE, 0 )
 
     for(height in minHeight..maxHeight) {
         var stackCount = 0
         var popCount = 0
 
-        for(i in 0 until n) {
-            for(j in 0 until m) {
-                val target = arr[i][j]
+        for(row in arr) {
+            for(target in row) {
                 when {
                     target < height -> stackCount += height - target
                     target > height -> popCount += target - height
@@ -47,9 +33,9 @@ fun main() {
 
         val time = stackCount + popCount * 2
 
-        if(b + popCount >= stackCount)
-            candidate = candidate.coerceAtLeast(Candidate(time, height))
+        if(b + popCount >= stackCount && time <= candidate.first)
+            candidate = Pair(time, height)
     }
 
-    with(candidate) { print("$time $height") }
+    with(candidate) { print("$first $second") }
 }
